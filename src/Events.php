844d828315fcfc7f11137b7f9d7c8aa4b5f90838
= <=>
2022-03-17 12:08:13 +0300

    First commit

---
+++ src/Events.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Events.php
+ *
+ * This file is part of Events.
+ *
+ * @author     Muhammet ŞAFAK <info@muhammetsafak.com.tr>
+ * @copyright  Copyright © 2022 Events
+ * @license    http://www.gnu.org/licenses/gpl-3.0.txt  GNU GPL 3.0
+ * @version    1.0
+ * @link       https://www.muhammetsafak.com.tr
+ */
+
+declare(strict_types=1);
+
+namespace InitPHP\Events;
+
+class Events
+{
+
+}
